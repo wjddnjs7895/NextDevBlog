c733731a8c2346507aebc36dffb764f511fcc40a
wjddnjs7895 <wjddnjs7895@naver.com>
2023-06-18 13:18:54 +0000

    feat: add useIsMobile custom hook

---
+++ src/utils/hooks.ts
@@ -0,0 +1,14 @@
+import theme from "@/styles/theme";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+  const mobile = useMediaQuery({
+    query: `(${theme.mobile})`,
+  });
+  useEffect(() => {
+    setIsMobile(mobile);
+  }, [mobile]);
+  return isMobile;
+}
